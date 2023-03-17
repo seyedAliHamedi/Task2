fcd53b9466ee1ca35f4490010b47cae49049c9b1
seyedAliHamedi <58143089+seyedAliHamedi@users.noreply.github.com>
2023-03-17 22:19:44 +0330

    Initial  files uploaded

---
+++ Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by seyedali hamedi on 3/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
