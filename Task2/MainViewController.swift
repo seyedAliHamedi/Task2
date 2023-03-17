8848b34144422ee94b6762a4439cc7d2d549fe50
Ali Hamedi <hamediseyedali@gmail.com>
2023-03-18 00:24:21 +0330

    setup google map sdk in app delegate and configure navigation controller in scene delgate

--- Task2/MainViewController.swift
+++ Task2/MainViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 class MainViewController: UIViewController {
 
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
 
-    }
+ 
+        
+        
+        
+}
 
 
+    
 }
 
